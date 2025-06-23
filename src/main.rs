647809410c1fd725aae708ef40f29630cc0347ca
unknown <tochidan11@gmail.com>
2025-06-23 09:49:03 +0100

    shadowing an immutable declared variable with variable of aanothe type even though same name

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,39 @@ fn main(){
     // }
 
     // println!("The value of x is: {}, and the value of y is: {}", x, y);
-    define_x()
+    // define_x()
 
+
+    // let  x:i32 = 10; // immutable variable
+
+    // {
+    //     let x= 12;
+    //     assert_eq!(x,12);
+    //     println!("The value of x in the inner scope is: {}", x);
+    // }
+
+    // assert_eq!(x,10);
+    // let x   = 42;
+    // println!("{}", x);
+
+
+
+    let mut x:i32 = 1;
+    x = 7;
+    //shaodwing and re-binding
+    // let x = x; // here will fail cos we initialise x and its by default immutable
+
+     x +=3;
+
+    let y:i32 = 4;
+
+    //shadowing
+    let _y = "I can also be bound to text";
+    println!("Success")
 }
 
-fn define_x(){
-    let x:&str = "Hello";
-    println!("{}, world", x);
+// fn define_x(){
+//     let x:&str = "Hello";
+//     println!("{}, world", x);
 
-}
+// }
