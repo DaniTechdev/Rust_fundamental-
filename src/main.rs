8d29492c46dd579be14f2d29ff463d7eab559430
unknown <tochidan11@gmail.com>
2025-06-29 10:28:10 +0100

    Array in Rust

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,30 @@ fn main(){
     //of the exact size of the array at compile time.
 
      
+
+    //  //fill the blank with proper arraay type
+    // let arr:[i32;5] = [1,2,3,4,5];
+
+
+
+    // //modify the code below to make it work
+
+    // assert!(arr.len() == 5);
+    // println!("success");
+
+
+    //We can ignore parts of the aarray types or even the whole type aand let the compiler infer it for us
+    // let arr0: [i32; 3] = [1,2,3];
+    // let arr:[char;3]= ['a','b','c'];
+
+    // //fill theblank
+    // //Array are stack allocated, `std::mem::size_of_val` returns the bytes which
+    // //A char takes 4 bytes in Rust: Unicode char
+
+    // assert!(std::mem::size_of_val(&arr) == 12);
+
+    // println!("success");
     
+
 }
 
