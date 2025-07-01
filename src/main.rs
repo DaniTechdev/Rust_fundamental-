bb3048881ecaeb7fe7a258c5c7ecbbac1d27810c
unknown <tochidan11@gmail.com>
2025-07-01 15:07:02 +0100

    Structs and exercises

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main(){
-//Struct
+    //Tuple Structs
+
+    
 }
---
+++ src/structs.rs
@@ -0,0 +1,65 @@
+fn main(){
+    //Struct
+    
+        // struct  User{
+        //     active: bool,
+        //     username:String,
+        //     email: String,
+        //     sign_in_count:u64
+        // }
+    
+    
+    
+        
+    //     struct  User{
+    //         active: bool,
+    //         username:String,
+    //         email: String,
+    //         sign_in_count:u64
+    //     }
+    
+        
+    //     let user1: User = User{
+    //         active:true,
+    //         username:String::from("natochi"),
+    //         email:String::from("natochi@gmail.com"),
+    //         sign_in_count:3,
+    
+    //     };
+    // // fn build_user(email:String,username:String) -> User{
+    // //     User{
+    // //         active:true,
+    // //         username,
+    // //         email,
+    // //         sign_in_count:1,
+    // //     }
+    // // }
+    
+    
+    //     // let user1 = build_user(String::from("tochi"), String::from("toch@gmail.com"));
+    
+    
+    //     // println!("this is user1 {:?}",user1);
+    
+    //     //Struct Updaate Syntax
+    
+    //     let user3: User = User{
+    //         active: user1.active,
+    //         username:user1.email,
+    //         email:String::from("another@example.com"),
+    //         sign_in_count:user1.sign_in_count,
+    //     };
+    
+    
+        // let user4: User = User{
+        //     email: String::from("tochi@gmail.com"),
+        //     ..user1
+        // };
+    
+    
+    
+    
+    
+    }
+    
+    
