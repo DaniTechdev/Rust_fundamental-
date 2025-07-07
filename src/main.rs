1750fe9821c0b4a7bf19fe0af56119bbc06f6f76
unknown <tochidan11@gmail.com>
2025-07-07 17:30:55 +0100

    traits as function paarameters

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,24 @@
-//Implement `fn summary` to make the code work
-//fix the erorr without removing any code line
 
-use std::iter::Sum;
+trait Animal {}
 
-trait Summary {
-    fn summarize(&self) -> String;
-}
+struct Dog;
+struct Cat;
 
+impl Animal for Dog{}
+impl Animal for Cat{}
 
-#[derive(Debug)]
-struct Post {
-    title:String,
-    author:String,
-    content:String
-}
-
-impl Summary for Post{
-    fn summarize(&self) -> String {
-        format!("The author of post {} is {}", self.title, self.author)
+fn return_animal(s:&str)-> &dyn Animal {
+    match s {
+        "dog"=> &Dog{},
+        "cat"=> &Cat{},
+        _ => panic!(),
     }
 }
 
-#[derive(Debug)]
-struct  Weibo{
-    username: String,
-    content:String
-}
-
-impl Summary for Weibo{
-    fn summarize(&self) -> String {
-        format!("{} published a weibo {}", self.username, self.content)
-    }
-}
 
 
 fn main(){
-    let post: Post = Post {
-        title:"Popular Rust".to_string(),
-        author:"Sunface".to_string(),
-        content:"Rust is awesome".to_string()
-    };
-
-    let weibo :Weibo = Weibo {
-        username: "sunface".to_string(), 
-        content: "Weibo seems to be worse than Tweet".to_string() 
-    };
-
-    summary(&post);
-    summary(&weibo);
-
-    println!("{:?}",post);
-    println!("{:?}", weibo);
-
-    println!("{} post.summarize", post.summarize());
+    let animal1 = return_animal("cat");
+    let animal2 = return_animal("dog");
 }
-
-
-//Implement `fn summary` below
-
-// fn summary(item: &impl Summary) {
-//  let output:String = item.summarize();
-
-//  println!("{}", output)
-// }
-
-//Or
-
-fn summary<T: Summary>(item: &T) {
-    
-    let output:String = item.summarize();
-   
-    println!("{}", output)
-   }
-
 
---
+++ src/traits_ex.rs
@@ -0,0 +1,40 @@
+
+struct  Sheep {}
+struct Cow{}
+
+
+trait Animal {
+    fn noice (&self)-> String;
+}
+
+impl Animal for Sheep{
+    fn noice (&self)-> String {
+        "baaaaaah".to_string()
+    }
+}
+
+impl Animal for Cow{
+    fn noice (&self)-> String {
+        "moooooo!".to_string()
+    }
+}
+
+
+//Returns some struct that implements Animal trait, but we dont't know which one at compile time
+//Fix the errors here, you can make a fake random, or you can use trait object.
+
+fn random_animal (random_number:f64) -> impl Animal{
+    if random_number < 0.5 {
+        Sheep{}
+    }else{
+        Cow{}
+    }
+}
+
+
+fn main(){
+    let random_number: f64 = 0.234;
+    let animal =  random_animal(random_number);
+
+    println!("You've randomly chosen an animal, it says {}", animal.noice())
+}
