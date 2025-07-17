0cdc9f40df2f909f2b9bef1a174a1ab9c00eb69e
unknown <tochidan11@gmail.com>
2025-07-17 15:14:41 +0100

    Result applied to reading and mutaing file using the file system

--- src/Unwrap_method.rs
+++ src/Unwrap_method.rs
@@ -1,24 +1,3 @@
-//Fill in the blank to display the whole call stack
-//Tip:you can find the clue in the default panic info
-
-
-fn divide(x:f32, y:f32) -> Result<f32,&'static str>{
-    if y == 0.0 {
-        return Err("Division by zero");
-    }
-    Ok(x/y)
-}
-
-fn main(){
-    let result: Result<f32, &'static str> = divide(10.0, 2.0);
-
-    match result {
-        Ok(val) => println!("Result: {}", val),
-        Err(msg)=> println!("Error: {}",msg)
-    }
-}
-
-
 //Fill in the blanks and fix the errors
 use std::num::ParseIntError;
 
--- src/parse_method.rs
+++ src/parse_method.rs
@@ -10,3 +10,24 @@ fn main(){
 
     println!("Success");
 }
+
+
+use std::num::ParseIntError;
+
+//Implement multiply with ?
+//Don't use unwrap here
+
+fn multiply(n1_str:&str, n2_str:&str) -> Result<i32, ParseIntError>{
+  let    n1: i32= n1_str.parse::<i32>()?; //Ok(3) -> 3
+  let    n2: i32= n2_str.parse::<i32>()?; //Ok(2)-> 4
+
+
+  Ok(n1 * n2) 
+
+
+}
+
+fn main(){
+    assert_eq!(multiply("3", "4").unwrap(), 12); //Ok(12) -> 12
+    println!("Success");
+}
