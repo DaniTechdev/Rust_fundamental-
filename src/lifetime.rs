147d4ee2de6410b19a71c1ff83707ef234c05dee
unknown <tochidan11@gmail.com>
2025-07-21 17:10:06 +0100

    lifetime notaation and references

---
+++ src/lifetime.rs
@@ -0,0 +1,47 @@
+fn main(){
+
+    // let i: i32 = 3;
+
+    // {
+    //     let borrow1: &i32 = &i;  //`borrow1` lifetime starts.
+    //     //
+    //     println!("borrow1: {}", borrow1);
+
+    // } //`borrow1 ends`
+
+    // {
+    //     let borrow2: &i32 = &i;
+
+    //     println!("borrow2: {}", borrow2)
+    // }
+
+
+    // {
+    //     let x = 5;
+    //     let r = &x;
+
+    //     println!("r: {}", r);
+    // }
+
+
+    // {
+
+    //     let r;
+
+    //     {
+    //         let x = 5;
+    //         r = &x;
+    //     }
+
+    //     println!("r: {}", r);
+    // }
+
+
+
+
+
+    
+
+}
+
+
